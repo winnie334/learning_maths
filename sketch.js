693352814ca27f62e59bf7b3d428bb81a0597147
winand appels <winnie33winnie33@gmail.com>
2018-03-08 12:01:40 +0100

    Made basically everything

--- sketch.js
+++ sketch.js
@@ -1,7 +1,72 @@
-function setup() {createCanvas(400, 400)
-  // put setup code here
+let nn, training_data, size;
+let canvas_size = 600;
+let selected = [-1, -1];
+
+function setup() {
+  size = 10;
+  createCanvas(canvas_size, canvas_size);
+  nn = new NeuralNetwork(2, 2, 1);
+  training_data = generateTrainingData(size);
+  console.log(training_data)
+}
+
+function generateTrainingData(number) {
+  var data = [];
+  for (var x = 0; x < number; x++) {
+    for (var y = 0; y < number; y++) {
+      object = {
+        inputs: [x, y],
+        outputs: [(x + y) / size / 2]
+      }
+      data.push(object);
+    }
+  }
+  return data;
 }
 
 function draw() {
- rect(20, 20, 20, 20) // put drawing code here
+  background(255);
+  displayInfo();
+  for (var i = 0; i < 1000; i++) {
+    train_object = random(training_data);
+    nn.train(train_object.inputs, train_object.outputs);
+  }
+
+  let columns = width / size;
+  let rows = height / size;
+  for (var i = 0; i < size; i++) {
+    for (var j = 0; j < size; j++) {
+      output = 2 * size * nn.predict([i, j]);
+      //text(str(output), i * columns, j * rows, [columns], [rows])
+      fill(map(output, 0, size * 2, 0, 255));
+      rect(i * columns, j * rows, columns, rows);
+    }
+  }
+}
+
+function displayInfo() {
+  if (selected[0] != -1 && selected[1] != -1) {
+    selected_output = 2 * size * nn.predict([selected[0], selected[1]]);
+    var s = selected[0] + " + " + selected[1] + " =";
+    document.getElementById("square").innerHTML = s;
+    document.getElementById("output").innerHTML = selected_output;
+  } else {
+    document.getElementById("square").innerHTML = "No square selected!";
+    document.getElementById("output").innerHTML = "";
+  }
+}
+
+function inbounds(x, y) {
+  if (mouseX < canvas_size && 0 <= mouseX && 0 <= mouseY && mouseY < canvas_size) {
+    return true;
+  }
+}
+
+function mouseClicked() {
+  if (inbounds(mouseX, mouseY)) {
+    selected[0] = Math.floor(mouseX / (canvas_size / size));
+    selected[1] = Math.floor(mouseY / (canvas_size / size));
+  } else {
+    selected = [-1, -1];
+  }
 }
